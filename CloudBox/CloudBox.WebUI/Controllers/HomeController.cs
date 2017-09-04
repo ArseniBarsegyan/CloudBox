7e8eb51df1cbffb12677529469c53d3a2c2413a5
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-04 20:29:27 +0300

    Added MVC4 project. Minimum of UI, authorize complete

---
+++ CloudBox/CloudBox.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace CloudBox.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+    }
+}
---
+++ CloudBox/CloudBox.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CloudBox.WebUI.MvcApplication" Language="C#" %>
