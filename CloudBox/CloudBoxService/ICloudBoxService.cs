d1fdb006e8a047c7d5e716de298f4ab042a24f8c
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-13 22:36:14 +0300

    Added WCF app with configured MySQL provider

---
+++ CloudBox/CloudBoxService/ICloudBoxService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace CloudBoxService
+{
+    [ServiceContract]
+    public interface ICloudBoxService
+    {
+        [OperationContract]
+        bool ValidatePassword(string username, string password);
+
+        [OperationContract]
+        bool UploadFilesToServer(IEnumerable<byte[]> filesCollection);
+    }
+}
