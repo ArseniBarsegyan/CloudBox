e403140a55c222d1d4a82402f66108edfcb3aec9
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-14 12:01:09 +0300

    WPFClient: validation logic updated

--- CloudBox/CloudBox.WPFClient/Menu/Login.xaml.cs
+++ CloudBox/CloudBox.WPFClient/Menu/Login.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using CloudBox.WPFClient.ServiceReference1;
 
 namespace CloudBox.WPFClient.Menu
 {
@@ -22,6 +23,28 @@ namespace CloudBox.WPFClient.Menu
         private void BackButton_OnClick(object sender, RoutedEventArgs e)
         {
             Switcher.Switch(new MainMenu());
+        }
+
+        //If validation successfull go to main page, else show message with validation error
+        private void SubmitButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (CloudBoxServiceClient serviceClient = new CloudBoxServiceClient())
+            {
+                var userName = UserNameField.Text;
+                var userPassword = UserPasswordField.Password;
+                if (userName != string.Empty && userPassword != string.Empty)
+                {
+                    bool result = serviceClient.ValidateUser(userName, userPassword);
+                    if (result)
+                    {
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect login or password");
+                    }
+                }
+            }
         }
     }
 }
