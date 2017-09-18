01a3db550949e197560fd54abf8fca51ac82f7bf
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-18 23:09:01 +0300

    WPF: DirectoryModel and FileModel changed

--- CloudBox/CloudBox.WPFClient/Models/DirectoryModel.cs
+++ CloudBox/CloudBox.WPFClient/Models/DirectoryModel.cs
@@ -4,6 +4,5 @@
     {
         public string Name { get; set; }
         public string CreationTime { get; set; }
-        public string ActionName { get; set; }
     }
 }
--- CloudBox/CloudBox.WPFClient/Models/FileModel.cs
+++ CloudBox/CloudBox.WPFClient/Models/FileModel.cs
@@ -5,6 +5,5 @@
         public string Name { get; set; }
         public string Extension { get; set; }
         public string CreationTime { get; set; }
-        public string ActionName { get; set; }
     }
 }
