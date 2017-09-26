53ed581ca2f04513972995dbc89f50fea55e49be
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-26 23:47:59 +0300

    Added CloudBox.WCFServiceHost. Console based host

---
+++ CloudBox/CloudBox.WCFServiceHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace CloudBox.WCFServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Console Based WCF Host *****");
+            using (ServiceHost serviceHost = new ServiceHost(typeof(CloudBox.WCFService.CloudService)))
+            {
+                serviceHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press the Enter key to terminate service.");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
