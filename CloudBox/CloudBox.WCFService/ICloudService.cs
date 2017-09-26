16f23c1407a257eadae91c42a1d5aa739352ce2c
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-26 23:47:20 +0300

    Added CloudBox.WCFService

---
+++ CloudBox/CloudBox.WCFService/ICloudService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace CloudBox.WCFService
+{    
+    [ServiceContract]
+    public interface ICloudService
+    {
+        [OperationContract]
+        bool ValidateUser(string username, string password);
+
+        [OperationContract]
+        IEnumerable<string> GetAllDirectoriesByPath(string userName, string path);
+
+        [OperationContract]
+        IEnumerable<string> GetAllFilesByPath(string userName, string path);
+
+        [OperationContract]
+        void CheckIfDirectoryWithUserNameExists(string username);
+
+        [OperationContract]
+        bool UploadFilesToServer(string userName, string password, byte[] fileContent);
+
+        [OperationContract]
+        string CreateDirectoryIfNotExists(string path);
+
+        [OperationContract]
+        void RemoveElement(string path);
+
+        [OperationContract]
+        string Upload(byte[] file, string path);
+
+        [OperationContract]
+        string GetFileLink(string path);
+    }
+}
